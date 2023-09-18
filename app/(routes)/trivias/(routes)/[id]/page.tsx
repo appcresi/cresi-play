5c922e8a2314ab61a36999571fb059fd15b1f7d2
Ticiano Morvan <ticiano.morvan@protonmail.com>
2023-09-18 11:44:49 -0300

    update: generate medatadata for trivias

--- app/(routes)/trivias/(routes)/[id]/page.tsx
+++ app/(routes)/trivias/(routes)/[id]/page.tsx
@@ -2,6 +2,7 @@ import { Trivia, TriviaQuestion } from "@/types/trivia";
 import { sortArrayRandomly } from "@/utils/array";
 import { API_URL } from "@/utils/server";
 import TriviaGame from "../../components/TriviaGame";
+import { Metadata } from "next";
 
 /** Return the trivia corresponding to the ID, from the back-end. */
 async function getTriviaById(id: string): Promise<Trivia> {
@@ -15,6 +16,17 @@ async function getTriviaById(id: string): Promise<Trivia> {
 	}
 
 	return body.data;
+}
+
+export async function generateMetadata({
+	params
+}: { params: { id: string } }): Promise<Metadata> {
+	const triviaData = await getTriviaById(params.id);
+
+	return {
+		title: `Aprendé sobre "${triviaData.name}" con nuestra trivia | CrESI`,
+		description: `Jugá a la trivia de "${triviaData.name}" y aprendé sobre el tema con una trivia interactiva y dinámica de preguntas y respuestas.`,
+	}
 }
 
 export default async function TriviaPage({
