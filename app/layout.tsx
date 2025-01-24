ccf265bbab023902c061cc01d7ddd4b0258fb21f
appcresi <appcresi@gmail.com>
2025-01-24 20:35:57 -0300

    Metadata del home

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,50 @@ const monaSans = localFont({
 import ComicBurgerMenu from '@/components/ComicBurgerMenu';
 
 export const metadata: Metadata = {
-  icons: ['/cresi-logo.ico']
-}
+  metadataBase: new URL("https://jugar.cresi.com.ar"),
+  title: "CrESI | Jugar",
+  icons: ['/cresi-logo.ico'],
+  description:
+    "Jugá a con nuestras trivias, completaspalabras, pasapalabras, y mucho más distribuidas en diferentes temáticas y niveles de dificultad. Aprendé mientras te divertís con CrESI.",
+  keywords: [
+    "CrESI",
+    "trivia",
+    "juegos educativos",
+    "trivia didácticas",
+    "educación sexual integral",
+    "trivia ESI",
+    "preguntas y respuestas",
+    "juegos para jóvenes",
+    "aprender jugando",
+  ],
+  openGraph: {
+    title: "CrESI | Jugar",
+    description:
+      "Jugá a con nuestras trivias, completaspalabras, pasapalabras, y mucho más distribuidas en diferentes temáticas y niveles de dificultad. Aprendé mientras te divertís con CrESI.",
+    url: "https://jugar.cresi.com.ar/trivias",
+    siteName: "CrESI",
+    images: [
+      {
+        url: "illustration-1.jpg",
+        width: 1200,
+        height: 630,
+        alt: "CrESI Jugar - Recursos didáticos para aprender jugando.",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "CrESI | Trivia",
+    description:
+      "Jugá a responder a más de 700 preguntas, distribuidas en diferentes temáticas y niveles de dificultad. Aprendé mientras te divertís con CrESI.",
+    images: ["illustration-1.jpg"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  }
+};
 
 export default function RootLayout ({ children }: { children: React.ReactNode }): JSX.Element {
   return (
