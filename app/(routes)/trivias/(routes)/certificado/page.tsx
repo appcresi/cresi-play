a850653f8518d7507f7022be6735a344f86e78f3
John Doe <appcresi@gmail.com>
2025-12-06 20:25:41 -0300

    fuera api3

--- app/(routes)/trivias/(routes)/certificado/page.tsx
+++ app/(routes)/trivias/(routes)/certificado/page.tsx
@@ -1,6 +1,9 @@
 import { type Metadata } from 'next'
 import TriviaCertificate from '../../components/TriviaCertificate'
 
+// Deshabilita el pre-renderamiento estático porque html2pdf solo funciona en el cliente
+export const dynamic = 'force-dynamic'
+
 export const metadata: Metadata = {
   title: 'Certificado de aprobación | CrESI',
   description: 'Descarga aquí tu certificado de aprobación de la trivia de CrESI',
